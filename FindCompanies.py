49b75fb52099dd3ad5680ba44247248debd58ef4
Poojitha@368 <poojithareddy2662004@gmail.com>
2025-10-03 19:03:43 +0530

    added fetching companies in dashboard.py

--- FindCompanies.py
+++ FindCompanies.py
@@ -1,6 +1,7 @@
 import requests
 import streamlit as st
 
+
 API_KEY = "d376tm1r01qtvbtkcj00d376tm1r01qtvbtkcj0g"
 URL = f"https://finnhub.io/api/v1/stock/symbol?exchange=US&token={API_KEY}"
 
@@ -13,17 +14,4 @@ def FetchCompanies():
     return companies
 
 def FilterCompanies(companies):
-    st.header("Choose a Company")
-
-    #serach with inputbox
-    '''search = st.text_input("search a company")
-    filtered = [company for company in companies if search.lower() in company.lower()]
-
-    company = st.selectbox("Matching Companies", filtered) if filtered else None
-
-    if company:
-        st.write(f"You selected {company}")
-    else:
-        st.write("No company found")
-    '''
-    return company
+    pass
