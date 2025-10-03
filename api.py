7fdd6a667b961905f1a125e6c9b19c94de9a4cdb
Poojitha@368 <poojithareddy2662004@gmail.com>
2025-10-03 19:03:24 +0530

    fetching data from api

--- api.py
+++ api.py
@@ -9,26 +9,34 @@ BASE_URL = "https://finnhub.io/api/v1/company-news"
 
 
 def FetchDataViaApi(company):
-    to_date = datetime.today()
-    from_date = to_date - timedelta(days=7)
-    params = {
-        'symbol': company,
-        'from': from_date.strftime('%Y-%m-%d'),
-        'to': to_date.strftime('%Y-%m-%d'),
-        'token': API_KEY
-    }
+    def getHeadlines(days):
+        to_date = datetime.today()
+        from_date = to_date - timedelta(days=days)
+        params = {
+            'description': company,
+            'from': from_date.strftime('%Y-%m-%d'),
+            'to': to_date.strftime('%Y-%m-%d'),
+            'token': API_KEY
+        }
 
-    response = requests.get(BASE_URL, params=params)
-    if response.status_code != 200:
-        return f"Error fetching news for {company}: {response.status_code}"
-    
-    news_data = response.json()
-    
-    if not news_data:
-        return "No news data found for the company last week"
-    
-    headlines = [record['headline'] for record in news_data]
+        response = requests.get(BASE_URL, params=params)
+        if response.status_code != 200:
+            return f"Error fetching news for {company}: {response.status_code}"
+        
+        news_data = response.json()
+        
+        headlines = [record['headline'] for record in news_data]
 
+
+        return headlines
+    
+
+    #try for 7 days
+    headlines = getHeadlines(7)
+    if not headlines:
+        # if no data for 7 days then try for last 30 days
+        headlines = getHeadlines(30)
+    
     return headlines
 
     
