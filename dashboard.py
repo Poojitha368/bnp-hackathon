80eda0b4e6c7b2a579ed18a1503afc2e91080609
Poojitha@368 <poojithareddy2662004@gmail.com>
2025-10-03 19:00:37 +0530

    updates of companies

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,7 @@ import threading
 from sentiment import FindSentiment
 from api import FetchDataViaApi
 from GenerateWordCloud import wordCloudImage
+from FindCompanies import FetchCompanies
 # from LLMrecommendation import BuySellRecommendation
 
 
@@ -15,22 +16,32 @@ st.set_page_config(
 
 
 st.title("📈 Real-Time Market Sentiment Dashboard")
+
+#selection of companies
 st.header("Choose a Company")
-companies = ["AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "NVDA", "META", "JPM", "NFLX", "DIS"]
-company = st.selectbox("Select Company", companies)
+# companies = ["AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "NVDA", "META", "JPM", "NFLX", "DIS"]
+companies = FetchCompanies()
+search = st.text_input("search a company")
+filtered = [company for company in companies if search.lower() in company.lower()]
+
+company = st.selectbox("Matching Companies", filtered) if filtered else None
+
+if company:
+    st.write(f"You selected {company}")
+
 
 #fetching news data via finhubb api
-# headlines = FetchDataViaApi(company)
-headlines = [
-    "Apple internally weighs bid for ChatGPT rival Perplexity, Bloomberg says",
-    "Apple (AAPL) Rises As Market Takes a Dip: Key Facts",
-    "Apple executives held internal talks about buying Perplexity, Bloomberg News reports",
-    "Stocks Settle Mostly Lower as Chip Makers Fall"
-]
+headlines = FetchDataViaApi(company)
+# headlines = [
+#     "Apple internally weighs bid for ChatGPT rival Perplexity, Bloomberg says",
+#     "Apple (AAPL) Rises As Market Takes a Dip: Key Facts",
+#     "Apple executives held internal talks about buying Perplexity, Bloomberg News reports",
+#     "Stocks Settle Mostly Lower as Chip Makers Fall"
+# ]
 
 #using threading for simultaneous execution
 if headlines:
-
+    
     #finding the sentiment scores
     sentiment,pos_count,neu_count,neg_count,final_score = FindSentiment(headlines,company)
     st.write("The sentiment score for ",company," out of 5 is ",final_score)
@@ -51,7 +62,10 @@ if headlines:
     st.subheader("News gatherings from various sources")
     newsGathers = headlines[:5]
     news_text = "\n\n".join(newsGathers)
-    st.write(news_text)
+    if news_text:
+        st.write(news_text)
+    else:
+        st.write("No news to show")
 
     
 
